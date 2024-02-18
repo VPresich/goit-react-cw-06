9fb28ded66a9647aca45b28853732da12970da59
VPresich <valya.alexandrova.presich@gmail.com>
2024-02-18 22:57:33 +0100

    example from module 6

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-
+import { store } from './jsx/redux/store';
+import { Provider } from 'react-redux';
 import './css/index.css';
 import { App } from './jsx/components/App';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>
 );
